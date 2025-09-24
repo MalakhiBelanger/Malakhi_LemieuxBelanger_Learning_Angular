98477aa2248f35ed74f4eb872228a3f5fcc71d9a
Malakhi Lemieux-Belanger <w0855776@myscc.ca>
2025-09-23 21:10:00 -0400

    Cards display
    Created 6 cards and displayed all of them on screen. Also made a header, a footer, and some CSS to make everything look alright.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Card } from './models/card';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ NgIf, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Malakhi_LemieuxBelanger_Learning_Angular';
-  name = 'Malakhi';
-  age = 23;
+  title: string = 'Malakhi_LemieuxBelanger_Learning_Angular';
+  name: string = 'Malakhi';
+  age: number = 23;
+  // Very long array
+  cardList: Card[] = [{id: 1, color: "devoid", name: "Black Lotus", oracle: "Tap, Sacrifice this artifact: Add three mana of any one color.", type: "Artifact", cost: "0"}, {id: 2, color: "blue", name: "Omniscience", type: "Enchantment", oracle: "You may cast spells from your hand without paying their mana costs.", cost: "{U}{U}{U}{7}"}, {id: 3, color: "white", type: "Enchantment", oracle: "Hexproof\nIf a source would deal damage to you, prevent that damage and put an incarnation counter on this enchantment.\nWhen there are nine or more incarnation counters on this enchantment, exile it.\nWhen this enchantment leaves the battlefield, you lose the game.", name: "Nine Lives", cost: "{W}{W}{1}"}, {id: 4, oracle: "Skip your draw step.\nWhenever you discard a card, exile that card from your graveyard.\nPay 1 life: Exile the top card of your library face down. Put that card into your hand at the beginning of your next end step.", name: "Necropotence", type: "Enchantment", color: "black", cost: "{B}{B}{B}"}, {id: 5, cost: "{G}", color: "green", type: "Instant", oracle: "As an additional cost to cast this spell, sacrifice a land.\nSearch your library for a land card, put that card onto the battlefield, then shuffle.", name: "Crop Rotation"}, {id: 6, name: "Underworld Breach", oracle: "Each nonland card in your graveyard has escape. The escape cost is equal to the card’s mana cost plus exile three other cards from your graveyard. (You may cast cards from your graveyard for their escape cost.)\nAt the beginning of the end step, sacrifice this enchantment.", type: "Enchantment", color: "red", cost: "{1}{R}"}];
 }
--- src/app/models/card.ts
+++ src/app/models/card.ts
@@ -1,8 +1,9 @@
-export interface Cards {
+export interface Card {
   id: number;
   color: string;
   name: string;
   type: string;
+  cost: string;
   oracle?: string;
   power?: string;
   toughness?: string;
