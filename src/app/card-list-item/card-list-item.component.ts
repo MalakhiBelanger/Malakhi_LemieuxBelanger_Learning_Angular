8d2a4cb1bca2f06f5bb936a55ee525fdd1cc1142
Malakhi Lemieux-Belanger <w0855776@myscc.ca>
2025-10-02 17:28:29 -0400

    New components
    * Added two new components: CardList and CardListItem. These will allow displaying cards onto the browser.

---
+++ src/app/card-list-item/card-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-list-item',
+  imports: [],
+  templateUrl: './card-list-item.component.html',
+  styleUrl: './card-list-item.component.css'
+})
+export class CardListItemComponent {
+
+}
