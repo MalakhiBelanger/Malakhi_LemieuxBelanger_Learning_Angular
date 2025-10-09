b7d1298cb7379eb1de7967db228b6af9ad988cfe
Malakhi Lemieux-Belanger <w0855776@myscc.ca>
2025-10-09 09:20:31 -0400

    MagicCardService CRUD
    * Added CRUD to the Service, it is currently untested.

--- src/app/services/magic-card.service.ts
+++ src/app/services/magic-card.service.ts
@@ -7,8 +7,35 @@ import {cardList} from '../data/mock-content';
   providedIn: 'root'
 })
 export class MagicCardService {
-  constructor() { }
+  private cards: Card[];
+  constructor() {
+    this.cards = cardList;
+  }
   getCards(): Observable<Card[]> {
-    return of(cardList);
+    return of(this.cards);
+  }
+  // Add a new card
+  addCard(newCard:Card) : Observable<Card[]> {
+    this.cards.push(newCard)
+    return of(this.cards);
+  }
+  // Update an Existing card
+  updateCard(updatedCard: Card): Observable<Card[]> {
+    const i = this.cards.findIndex(card => card.id === updatedCard.id)
+    if(i !== -1) {
+      this.cards[i] = updatedCard
+    }
+    return of(this.cards);
+  }
+  // Delete: Remove a card by ID
+  deleteCard(cardId: number): Observable<Card[]> {
+    this.cards = this.cards.filter(card => card.id !== cardId);
+    return of(this.cards);
+  }
+  // Get card by ID
+  getCardById(cardId: number): Observable<Card> {
+    const card = this.cards.find(card => card.id === cardId)
+    // @ts-ignore
+    return of(card)
   }
 }
