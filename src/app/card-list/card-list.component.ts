b24e454ff9388ab14551ee26d9efff4e18dfe5c1
Malakhi Lemieux-Belanger <w0855776@myscc.ca>
2025-10-09 08:42:34 -0400

    Card List Component
    * Updated the list component so that it accepts the service.

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Card} from '../models/card';
 import {CardListItemComponent} from '../card-list-item/card-list-item.component';
 import {NgForOf} from '@angular/common';
+import { MagicCardService } from '../services/magic-card.service';
 
 @Component({
   selector: 'app-card-list',
@@ -12,7 +13,16 @@ import {NgForOf} from '@angular/common';
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.css'
 })
-export class CardListComponent {
-
+export class CardListComponent implements OnInit {
+  private magicCardService: MagicCardService;
+  protected cardList: Card[] = [];
+  constructor(private cardService: MagicCardService) {
+    this.magicCardService = cardService
+  }
+  ngOnInit() {
+    this.magicCardService.getCards().subscribe({
+      next: (data:Card[]) => this.cardList = data, error:err => console.error("Error fetching", err), complete:() => console.log("fetch complete!")
+    })
+  }
 }
 
--- src/app/services/magic-card.service.ts
+++ src/app/services/magic-card.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import {Card} from '../models/card';
+import {cardList} from '../data/mock-content';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MagicCardService {
-
   constructor() { }
+  getCards(): Observable<Card[]> {
+    return of(cardList);
+  }
 }
